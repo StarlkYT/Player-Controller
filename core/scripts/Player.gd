d2dddbea4707199f8c886f2bc6b589d2c4382059
StarlkYT <45287860+StarlkYT@users.noreply.github.com>
2020-12-30 01:08:39 +0300

    Create Player.gd

---
+++ core/scripts/Player.gd
@@ -0,0 +1,61 @@
+"""
+THANKS to PigDev for helping me to fix the slope sliding related problems
+and the Godot engine community for helping!
+"""
+
+extends KinematicBody2D
+
+# Keyboard controlling from Input Map
+export (String) var move_right_key = "ui_right"
+export (String) var move_left_key = "ui_left"
+export (String) var jump_key = "ui_up"
+
+# Player movement settings
+export (int) var speed = 128
+export (int) var gravity = 256
+export (int) var jump_power = 256
+export (int) var snap_length = 32
+export (int) var max_floor_degree = 45
+
+# Smoothing-related variables for player movement
+export (int) var ground_friction = 15
+export (int) var air_friction = 5
+
+onready var player_sprite: Sprite = $CollisionShape2D/Sprite
+onready var player_on_floor: RayCast2D = $OnFloor
+
+var double_jump: bool = false
+var friction: int = ground_friction
+var snap_direction: Vector2 = Vector2.DOWN
+var snap_vector: Vector2 = snap_direction * snap_length
+var motion: Vector2 = Vector2()
+
+func get_horizontal_input() -> float:
+	return (Input.get_action_strength(move_right_key) - Input.get_action_strength(move_left_key))
+
+func _physics_process(delta: float) -> void:
+	if is_on_floor():
+		friction = ground_friction
+	else:
+		friction = air_friction
+	apply_jump()
+	apply_gravity(delta)
+	apply_movement(delta)
+	motion.y = move_and_slide_with_snap(motion, snap_vector, Vector2.UP, deg2rad(max_floor_degree)).y
+	
+func apply_movement(delta: float) -> void:
+	if get_horizontal_input() != 0:
+		player_sprite.flip_h = get_horizontal_input() < 1
+	motion.x = get_horizontal_input() * speed
+
+func apply_jump() -> void:
+	if Input.is_action_just_pressed(jump_key) and player_on_floor.is_colliding():
+		snap_vector = Vector2.UP
+		motion.y = -jump_power
+	else:
+		snap_vector = snap_direction * snap_length
+
+func apply_gravity(delta: float) -> void:
+	motion.y += gravity * delta
+	if motion.y > 500:
+		motion.y = 500
